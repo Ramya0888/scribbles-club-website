d92e65e3f97cd13b7297a320cf54038b7530ee55
Senthil Raja R <senthilrajasen637@gmail.com>
2026-09-14 11:20:51 +0530

    fix: update .gitignore for backend files, modify ESLint config, and add team image

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,6 @@ import globals from 'globals';
 export default [
   js.configs.recommended,
   { languageOptions: { globals: { ...globals.browser, ...globals.node }, ecmaVersion: 'latest', sourceType: 'module', parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { files: ['**/*.{js,jsx}'], plugins: { react }, rules: { 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], 'react/prop-types': 'off', 'no-undef': 'off' }, settings: { react: { version: 'detect' } } },
+  { files: ['**/*.{js,jsx}'], plugins: { react }, rules: { 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], 'react/prop-types': 'off' }, settings: { react: { version: 'detect' } } },
   { ignores: ['dist/', 'node_modules/'] },
 ];
